91601f6e0e84681038de415eeb227193cc788c8b
Michel Briand <michel.briand.fr@gmail.com>
2025-02-04 14:23:45 +0100

    exporter des sources yaml: besoin de préserver l'indentation

--- emacs/org-mode_batch_init.el
+++ emacs/org-mode_batch_init.el
@@ -1,5 +1,6 @@
 (package-initialize)
 (require 'ox-beamer)
 (setq org-beamer-outline-frame-title "Plan")
+(setq org-src-preserve-indentation t)
 (load-file ".emacs.d/org-mode_ansible-listings.el")
 
