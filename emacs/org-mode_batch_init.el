00ca1f979b0e0cdfa86cd621d28695e489568c4d
Michel Briand <michel.briand.fr@gmail.com>
2025-02-05 22:14:56 +0100

    export latex : commande plus robuste

--- emacs/org-mode_batch_init.el
+++ emacs/org-mode_batch_init.el
@@ -1,6 +1,6 @@
 (package-initialize)
 (require 'ox-beamer)
+(setq org-latex-pdf-process '("latexmk -f -pdf -%latex -interaction=nonstopmode -output-directory=%o %f"))
 (setq org-beamer-outline-frame-title "Plan")
 (setq org-src-preserve-indentation t)
 (load-file ".emacs.d/org-mode_ansible-listings.el")
-
