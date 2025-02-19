53c01f618771bd17deec0b70058af03b196e8d0e
Michel Briand <michel.briand.fr@gmail.com>
2025-02-19 16:22:26 +0100

    nouvelles couleurs pour Ansible

--- emacs/org-mode_batch_init.el
+++ emacs/org-mode_batch_init.el
@@ -3,4 +3,4 @@
 (setq org-latex-pdf-process '("latexmk -f -pdf -%latex -interaction=nonstopmode -output-directory=%o %f"))
 (setq org-beamer-outline-frame-title "Plan")
 (setq org-src-preserve-indentation t)
-(load-file ".emacs.d/org-mode_ansible-listings.el")
+
